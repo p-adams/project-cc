e1d205fe69e174071dbf4fd8103d8786e87ce185
Paul Adams <ghusiyy@gmail.com>
2022-08-31 23:40:40 -0700

    add mp4 and vtt file assets to server

--- demo-server/server.js
+++ demo-server/server.js
@@ -1,12 +1,38 @@
 const Hapi = require("@hapi/hapi");
+const Path = require("path");
+//const captions = require("./vtt_files/en.vtt");
 
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
     host: "localhost",
+    routes: {
+      files: {
+        relativeTo: Path.join(__dirname, "assets"),
+      },
+    },
+  });
+
+  await server.register(require("@hapi/inert"));
+
+  server.route({
+    method: "GET",
+    path: "/vtt_files/en.vtt",
+    handler: function (request, h) {
+      return h.file("vtt_files/en.vtt");
+    },
+  });
+
+  server.route({
+    method: "GET",
+    path: "/videos/waves.mp4",
+    handler: function (request, h) {
+      return h.file("videos/waves.mp4");
+    },
   });
 
   await server.start();
+  // console.log(captions);
   console.log("Server running on %s", server.info.uri);
 };
 
